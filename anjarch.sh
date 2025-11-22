a74c68c4f01ade261241d291b5b3b075513566a3
Flan-Angel <flan.angel@proton.me>
2025-11-22 21:17:59 +0530

    Add files via upload

--- anjarch.sh
+++ anjarch.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/emv sh
 set -euo pipefail
+clear
+echo "Anjelo's Arch installation Script"
+sleep 3s 
+clear
+read -p "Pick disk to install on (eg: /dev/sdx)" USER_DISK
+lsblk
 
-echo "nerd"
+sfdisk $USER_DISK
+
+clear
+lsblk
+read -p "name root partition (/dev/sdx)" ROOT_PART
+mkfs.ext4 $ROOT_PART
+read -p "name swap partition (/dev/sdx)" SWAP_PART
+mkswap $SWAP_PART
+
+mount $ROOT_PART /mnt
+swapon $SWAP_PART
+
+pacstrap -K 
