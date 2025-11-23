ead3750e29b5034a48413cc8f2df363997c320dd
Flan-Angel <flan.angel@proton.me>
2025-11-23 21:00:20 +0530

    Add files via upload

--- anjarch.sh
+++ anjarch.sh
@@ -4,18 +4,20 @@ clear
 echo "Anjelo's Arch installation Script"
 sleep 3s 
 clear
-read -p "Pick disk to install on (eg: /dev/sdx)" USER_DISK
-
-#sfdisk $USER_DISK
-
-clear
-lsblk
-read -p "name root partition (/dev/sdx)" ROOT_PART
-mkfs.ext4 $ROOT_PART
-read -p "name swap partition (/dev/sdx)" SWAP_PART
-mkswap $SWAP_PART
-
-mount $ROOT_PART /mnt
-swapon $SWAP_PART
-echo "cumpy cumpy"
-#pacstrap -K 
+echo "Pick disk to install on (eg: /dev/sdx)"
+read USER_DISK
+echo $USER_DISK
+#
+##sfdisk $USER_DISK
+#
+#clear
+#lsblk
+#read -p "name root partition (/dev/sdx)" ROOT_PART
+#mkfs.ext4 $ROOT_PART
+#read -p "name swap partition (/dev/sdx)" SWAP_PART
+#mkswap $SWAP_PART
+#
+#mount $ROOT_PART /mnt
+#swapon $SWAP_PART
+#echo "cumpy cumpy"
+##pacstrap -K 
